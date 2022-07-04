cb53454554c318d3a863980eb98919fd70dd59dd
RiJJCh <ricjich@gmail.com>
2022-07-04 14:23:38 -0500

    BTN´s de aunmentar y disminuir

--- componentes/src/Tarjeta.js
+++ componentes/src/Tarjeta.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Tarjeta(props) {
 
-    let experiencia=5
+    const [años,setAños]=useState(5)
+
+    function aumentarAños(){
+        setAños(años+1)
+    }
+
+    function disminuirAños(){
+        setAños(años-1)
+    }
 
     return(
         <div>
             <h1>{props.Nombre}</h1>
             <h2>{props.Ocupacion}</h2>
-            <p>Años de experiencia: {experiencia} años</p>
+            <p>Años de experiencia: {años} años</p>
             <p>Actualmente se encuentra estudiando en la Ucreativa.</p>
+            <button onClick={aumentarAños}>Aumentar años</button>
+            <button onClick={disminuirAños}> Disminuir años</button>
         </div>
     )
 }
